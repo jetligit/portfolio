55d2ba3790b6c484b402b89c2be089514e4506fb
Jet Li <jel7426@g.rit.edu>
2026-01-14 00:47:58 -0500

    changed logic to put project buttons and skills buttons on the main page

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function goToPage(page_name){
     window.location.href = page_name;
 }
+
+document.querySelectorAll('.project-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const link = button.dataset.link;
+        goToPage(link);
+    });
+});
