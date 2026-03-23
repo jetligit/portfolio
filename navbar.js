53cf5eba00215027977c5eedee051e54ff4485f1
Jet Li <jel7426@g.rit.edu>
2026-03-22 23:53:46 -0400

    Edited main page layout and assigned javascript header to all pages

---
+++ navbar.js
@@ -0,0 +1,18 @@
+function createHeader(containerId){
+    const container = document.getElementById(containerId);
+    if (!container) return;
+
+    container.innerHTML = `
+        <div class="top">
+            <h1>Welcome to Jet's Portfolio</h1>
+            <nav>
+                <ul>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/about/about.html">About</a></li>
+                    <li><a href="/projects/projects.html">Projects</a></li>
+                    <li><a href="/contact/contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    `;
+}
